31e07dfbd0e24665d9446c871257c0e2ccb45c58
Swetha Sakthi <swethasakthi927@gmail.com.com>
2025-05-08 15:13:12 +0530

    dd

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 const app = express();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 4000;
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
+
+const MONGO_URL = process.env.MONGO_URL;
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
