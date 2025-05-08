75b627eb367dc5ca467562c41477674340d8a333
Swetha Sakthi <swethasakthi927@gmail.com.com>
2025-05-08 16:24:37 +0530

    aa

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
 import express from "express";
+const app = express();
 import { MongoClient } from "mongodb";
-import * as dotenv from "dotenv";
 import usersRouter from "./routes/users.route.js";
+
+import * as dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
+app.get("/", function (request, response) {
+  response.send("🙋‍♂️, 🌏 🎊✨🤩");
+});
 
 const MONGO_URL = process.env.MONGO_URL;
 const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log("Mongo is connected successfully!");
+app.use("/users", usersRouter);
 
-async function startServer() {
-  try {
-    await client.connect();
-    console.log("Mongo is connected successfully!");
-
-    // Middleware
-    app.use(express.json()); // To parse JSON request body
-    app.use("/users", usersRouter); // Register routes for users
-
-    // Default Route
-    app.get("/", (req, res) => {
-      res.send("🙋‍♂️, 🌏 🎊✨🤩");
-    });
-
-    app.listen(PORT, () => {
-      console.log(`The server started in: ${PORT} ✨✨`);
-    });
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-  }
-}
-
-startServer();
-
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 export { client };
