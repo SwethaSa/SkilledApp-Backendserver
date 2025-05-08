86d8fd1e3e19d1ed28d28a3603bc7b0e02693a25
Swetha Sakthi <swethasakthi927@gmail.com.com>
2025-05-08 15:17:52 +0530

    ss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -9,5 +10,8 @@ app.get("/", function (request, response) {
 });
 
 const MONGO_URL = process.env.MONGO_URL;
+const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log("Mongo is connected successfully!");
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
